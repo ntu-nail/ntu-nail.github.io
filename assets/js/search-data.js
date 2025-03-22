97f06a1f7ec3c56b1cc7243ca5cfc7daeef05ed6
Elfsong <dumingzhex@gmail.com>
2025-03-22 18:33:51 +0000

    Deploying to gh-pages from @ ntu-nail/ntu-nail.github.io@a1875882e2a94adc5db8f725ccb208194ab9394a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-people",
           title: "People",
-          description: "Members of our lab",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
